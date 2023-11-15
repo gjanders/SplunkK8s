19b8aeada84ce3a00dbc5fe3eaeb4b7d3eb3b238
gjanders <gareth.anderson03@gmail.com>
2023-11-15 11:08:05 +1100

    Update k8s_healthcheck.sh

--- k8s_healthcheck.sh
+++ k8s_healthcheck.sh
@@ -10,6 +10,8 @@ current_time=$(date +%s)
 echo "$date results below" >> ${log}
 cat /tmp/combined.txt >> ${log}
 
+tmp_cm=/tmp/tmp_cm.yml
+
 while IFS= read -r line; do
     # Process each line here (e.g., print it)
     node=`echo $line | cut -d " " -f1`
@@ -45,9 +47,26 @@ while IFS= read -r line; do
                     sleep 3
                 fi
             done
+            # unfortunately re-creating the CM pod is not enough as the apps in etc/manager-apps etc/apps don't get re-deployed by the appframework
+            # to force this to occur we delete the cluster manager CRD and re-create it again with the same configuration (this triggers a rolling restart of the indexer
+            # cluster but it doesn't result in missing configuration)
+            cluster_manager=`kubectl get clustermanager -n $namespace -o name`
+            echo "$date kubectl get clustermanager -n $namespace -o name result is $cluster_manager" >> ${log}
+            kubectl get $cluster_manager -n $namespace -o yaml > ${tmp_cm}
+            echo "$date kubectl delete $cluster_manager -n $namespace" >> ${log}
+            kubectl delete $cluster_manager -n $namespace 2>&1 >> ${log}
+            statefulset=splunk-`echo $cluster_manager | cut -d "/" -f2`-cluster-manager
+            res=`kubectl get statefulset $statefulset -n $namespace`
+            if [ "x$res" != "x" ]; then
+                echo "$date kubectl delete statefulset $statefulset -n $namespace" >> ${log}
+                kubectl delete statefulset $statefulset -n $namespace 2>&1 >> ${log}
+            fi
             the_pod=`echo $pods | awk '{ print $2 }'`
             echo "$date kubectl delete pod -n $namespace ${the_pod}" >> ${log}
             kubectl delete pod -n $namespace ${the_pod} --grace-period 0 --force 2>&1 >> ${log}
+            sleep 10
+            echo "$date kubectl apply -f ${tmp_cm} -n $namespace" >> ${log}
+            kubectl apply -f ${tmp_cm} -n $namespace 2>&1 >> ${log}
         else
             echo "$date No cluster managers found on $node" >> ${log}
         fi
