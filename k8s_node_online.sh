aa1147c859ac71888828de539358dc12e23c7f16
gjanders <gareth.anderson03@gmail.com>
2024-02-05 14:37:55 +1100

    Update k8s_node_online.sh

--- k8s_node_online.sh
+++ k8s_node_online.sh
@@ -8,4 +8,8 @@ sleep 60
 echo $date kubectl uncordon `hostname` > ${log}
 kubectl uncordon `hostname` 2>&1 | tee -a ${log}
 
-
+# uncordon twice will work if it took an unusually long period of time to get everything online
+# and will do no harm
+sleep 120
+echo $date kubectl uncordon `hostname` (round 2) > ${log}
+kubectl uncordon `hostname` 2>&1 | tee -a ${log}
