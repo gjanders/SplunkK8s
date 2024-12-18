605f20e2ef46129625c648cec97ddb5c86453567
Gareth Anderson <gareth.anderson03@gmail.com>
2024-12-18 12:36:20 +1100

    Update k8s_node_offlinev2.sh

--- k8s_node_offlinev2.sh
+++ k8s_node_offlinev2.sh
@@ -6,11 +6,14 @@ log=/opt/splunkforwarder/var/log/splunk/k8s_node_offline.sh
 
 echo $date kubectl cordon `hostname` > ${log}
 kubectl cordon `hostname` 2>&1 | tee -a ${log}
+
 kubectl get pods -o wide -A | grep $(hostname) | grep "splunk-" > /tmp/pod_output.txt
 while IFS= read -r line; do
     echo $date $line is running on this node | tee -a ${log}
     cm=`echo $line | grep "cluster-manager"`
     indexer=`echo $line | grep "indexer"`
+    searchhead=`echo $line | grep "search-head"`
+    splunk=`echo $line | grep " splunk-"`
     namespace=`echo $line | awk '{ print $1 }'`
     pod=`echo $line | awk '{ print $2 }'`
     if [ "x$indexer" != "x" ]; then
@@ -20,6 +23,14 @@ while IFS= read -r line; do
     elif [ "x$cm" != "x" ]; then
         echo "$date $pod is a cluster manager in namespace $namespace. Calling /root/scripts/offline_remove_pod.sh clustermanager $namespace $pod" | tee -a ${log}
         /root/scripts/offline_remove_pod.sh "clustermanager" "$namespace" "$pod" &
+        pids+=($!)
+    elif [ "x${searchhead}" != "x" ]; then
+        echo "$date $pod is a search head in namespace $namespace. Calling /root/scripts/offline_remove_pod.sh searchhead $namespace $pod" | tee -a ${log}
+        /root/scripts/offline_remove_pod.sh "searchhead" "$namespace" "$pod" &
+        pids+=($!)
+    elif [ "x$splunk" != "x" ]; then
+        echo "$date $pod is a splunk instance in namespace $namespace. Calling /root/scripts/offline_remove_pod.sh splunk $namespace $pod" | tee -a ${log}
+        /root/scripts/offline_remove_pod.sh "splunk" "$namespace" "$pod" &
         pids+=($!)
     fi
     #echo namespace is $namespace
