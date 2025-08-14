dbe35724b8491eedda215dade8e029faa844417d
Gareth Anderson <gareth.anderson03@gmail.com>
2025-08-14 11:25:57 +1000

    Update k8s_node_offlinev2.sh

--- k8s_node_offlinev2.sh
+++ k8s_node_offlinev2.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 export KUBECONFIG=/etc/kubernetes/admin.conf
 
-date=`/bin/date +"%Y-%m-%d %H:%M:%S.%3N %z"`
-log=/opt/splunkforwarder/var/log/splunk/k8s_node_offline.sh
+LOG_FILE=/opt/splunkforwarder/var/log/splunk/k8s_node_offline.sh
 
-echo $date kubectl cordon `hostname` > ${log}
-kubectl cordon `hostname` 2>&1 | tee -a ${log}
+# Logging function
+log() {
+    local message="$1"
+    if [ -f "$LOG_FILE" ]; then
+        local log_size
+        log_size=$(stat -c%s "$LOG_FILE")
+        if [ "$log_size" -ge "$MAX_LOG_SIZE" ]; then
+            mv "$LOG_FILE" "${LOG_FILE}.1"
+        fi
+    fi
+    echo "$(date +'%Y-%m-%d %H:%M:%S.%3N %z') - $message" | tee -a "$LOG_FILE"
+}
+
+log kubectl cordon `hostname`
+kubectl cordon `hostname` 2>&1 | tee -a ${LOG_FILE}
 
 kubectl get pods -o wide -A | grep $(hostname) | grep "splunk-" > /tmp/pod_output.txt
 while IFS= read -r line; do
-    echo $date $line is running on this node | tee -a ${log}
+    log "$line is running on this node"
     cm=`echo $line | grep "cluster-manager"`
     indexer=`echo $line | grep "indexer"`
     searchhead=`echo $line | grep "search-head"`
@@ -17,19 +29,19 @@ while IFS= read -r line; do
     namespace=`echo $line | awk '{ print $1 }'`
     pod=`echo $line | awk '{ print $2 }'`
     if [ "x$indexer" != "x" ]; then
-        echo "$date $pod is an indexer in namespace $namespace. Calling /root/scripts/offline_remove_pod.sh indexer $namespace $pod" | tee -a ${log}
+        log "$pod is an indexer in namespace $namespace. Calling /root/scripts/offline_remove_pod.sh indexer $namespace $pod"
         /root/scripts/offline_remove_pod.sh "indexer" "$namespace" "$pod" &
         pids+=($!)
     elif [ "x$cm" != "x" ]; then
-        echo "$date $pod is a cluster manager in namespace $namespace. Calling /root/scripts/offline_remove_pod.sh clustermanager $namespace $pod" | tee -a ${log}
+        log "$pod is a cluster manager in namespace $namespace. Calling /root/scripts/offline_remove_pod.sh clustermanager $namespace $pod"
         /root/scripts/offline_remove_pod.sh "clustermanager" "$namespace" "$pod" &
         pids+=($!)
     elif [ "x${searchhead}" != "x" ]; then
-        echo "$date $pod is a search head in namespace $namespace. Calling /root/scripts/offline_remove_pod.sh searchhead $namespace $pod" | tee -a ${log}
+        log "$pod is a search head in namespace $namespace. Calling /root/scripts/offline_remove_pod.sh searchhead $namespace $pod"
         /root/scripts/offline_remove_pod.sh "searchhead" "$namespace" "$pod" &
         pids+=($!)
     elif [ "x$splunk" != "x" ]; then
-        echo "$date $pod is a splunk instance in namespace $namespace. Calling /root/scripts/offline_remove_pod.sh splunk $namespace $pod" | tee -a ${log}
+        log "$pod is a splunk instance in namespace $namespace. Calling /root/scripts/offline_remove_pod.sh splunk $namespace $pod"
         /root/scripts/offline_remove_pod.sh "splunk" "$namespace" "$pod" &
         pids+=($!)
     fi
@@ -43,8 +55,8 @@ for pid in "${pids[@]}"; do
 done
 
 for i in "${!status[@]}"; do
-    echo "$date job $i exited with ${status[$i]}" | tee -a ${log}
+    log "job $i exited with ${status[$i]}"
 done
 
-echo "$date kubectl drain `hostname` --ignore-daemonsets" | tee -a ${log}
-kubectl drain `hostname` --ignore-daemonsets 2>&1 | tee -a ${log}
+log "kubectl drain `hostname` --ignore-daemonsets"
+kubectl drain `hostname` --ignore-daemonsets 2>&1 | tee -a ${LOG_FILE}
